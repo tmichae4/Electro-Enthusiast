a28c00fd387d413891f7c3c9cc5a475f19a4651a
bdiaz28 <brittanymdiaz@yahoo.com>
2021-11-06 14:35:49 -0400

    commentRoutes

--- models/index.js
+++ models/index.js
@@ -14,19 +14,19 @@ Post.belongsTo(User, {
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
-});
-
-Comment.belongsTo(Post, {
+  });
+  
+  Comment.belongsTo(Post, {
     foreignKey: 'post_id'
-});
-
-User.hasMany(Comment, {
+  });
+  
+  User.hasMany(Comment, {
     foreignKey: 'user_id'
-});
-
-Post.hasMany(Comment, {
+  });
+  
+  Post.hasMany(Comment, {
     foreignKey: 'post_id'
-});
+  });
 
 module.exports = { User, Post, Comment };
 
