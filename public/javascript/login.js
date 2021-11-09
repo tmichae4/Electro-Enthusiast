f04b4963a2a09991d9e162897eaa4bc43eb25920
bdiaz28 <brittanymdiaz@yahoo.com>
2021-11-08 20:50:05 -0500

    style update

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -25,30 +25,30 @@ async function signupFormHandler(event){
         }
       }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
 
 async function loginFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-          method: 'post',
-          body: JSON.stringify({
-            email,
-            password
-          }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-    
-        if (response.ok) {
-          document.location.replace('/');
-        } else {
-          alert(response.statusText);
-        }
+  if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
       }
     }
+  }
 
-document.querySelector('.login-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
