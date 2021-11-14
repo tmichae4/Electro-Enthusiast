87e4e86d29d0ad4597109754c1e2c6230fa8980c
bdiaz28 <brittanymdiaz@yahoo.com>
2021-11-14 01:15:49 -0500

    err

--- models/post.js
+++ models/post.js
@@ -16,7 +16,7 @@ Post.init(
             allowNull: false
         },
         post_content: {
-            type: DataTypes.TEXT,
+            type: DataTypes.STRING,
             allowNull: false
         },
         user_id: {
